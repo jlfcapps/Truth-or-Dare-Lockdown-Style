c9e026c4753d8f370dcd4fc7ceede772ddd18c5c
jlfcapps <jlfc1988@gmail.com>
2020-10-14 12:30:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ install_reqs = parse_requirements('requirements.txt', session=False)
 dependencies = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='alexa-truth-or-dare-unrated',
+    name='truth-or-dare-lockdownstyle',
     version=version,
-    url='https://github.com/breboulet/alexa-truth-or-dare',
+    url='https://github.com/jlfcapps/Truth-or-Dare-Lockdown-Style',
     license='MIT',
-    author='Bastien Reboulet',
-    author_email='bastien.reboulet@gmail.com',
-    description='An Alexa skill to play Truth or Dare.',
+    author='JOSE LUIS FLOREZ CASTRO',
+    author_email='jlfc1988@gmail.com',
+    description='An Alexa skill to play Truth or Dare Lockdown Style.',
     packages=['tod'],
     include_package_data=True,
     install_requires=dependencies,
